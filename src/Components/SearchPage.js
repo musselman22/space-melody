2796388f1050f5b7981efd340247190d7bdb7590
Jacob Musselman <musselmj@oregonstate.edu>
2021-06-14 16:01:35 -0600

    add search component

---
+++ src/Components/SearchPage.js
@@ -0,0 +1,18 @@
+import TextField from '@material-ui/core/TextField';
+
+function SearchPage() {
+  return (
+    <>
+      <h1>Space Melody</h1>
+
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </>
+  )
+}
+
+export default SearchPage;
+
+//  https://material-ui.com/components/text-fields/ 
+
+// https://material.io/components/text-fields
+
